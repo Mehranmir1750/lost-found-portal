40ff969c6e9eed5c0ef59c8dc0729480df6164b4
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-03-14 07:06:53 +0530

    fix start script

--- db.js
+++ db.js
@@ -1,22 +1,20 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
-
 dotenv.config();
+
 const { Pool } = pg;
 
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // optional test
 pool.on("connect", () => {
-  console.log("Database connected (Pool)");
+  console.log("Database connected (Neon)");
 });
 
 export default pool;
